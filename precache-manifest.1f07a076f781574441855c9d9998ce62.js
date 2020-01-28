df9001b6f27accb39ecfecd8aafd5365a32392d8
José <zeehtecnologia@gmail.com>
2020-01-28 17:46:10 -0300

    Updates

---
+++ precache-manifest.1f07a076f781574441855c9d9998ce62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90a39f2523ca92f40746ca0e4d09981",
+    "url": "/amcom-test-frontend/index.html"
+  },
+  {
+    "revision": "c142083fae1c9218227e",
+    "url": "/amcom-test-frontend/static/css/main.a9afbf18.chunk.css"
+  },
+  {
+    "revision": "19a9eb5f72a30c06526b",
+    "url": "/amcom-test-frontend/static/js/2.39c6080f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/amcom-test-frontend/static/js/2.39c6080f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c142083fae1c9218227e",
+    "url": "/amcom-test-frontend/static/js/main.08be68b0.chunk.js"
+  },
+  {
+    "revision": "7b19edef4abd8afd7b48",
+    "url": "/amcom-test-frontend/static/js/runtime-main.908383e6.js"
+  }
+]);
